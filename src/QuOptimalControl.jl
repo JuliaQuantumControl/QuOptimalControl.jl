8644c46ed8802e5bd0f77fbaaa5f0308c23421b8
Alastair Marshall <alastair@nvision-imaging.com>
2020-08-12 19:04:27 +0200

    turning it into something that is approaching a real module

--- src/QuOptimalControl.jl
+++ src/QuOptimalControl.jl
@@ -1,7 +1,17 @@
 module QuOptimalControl
 
 # more to come
-include("./cost_functions.jl")
+include("cost_functions.jl")
+include("tools.jl")
+include("evolution.jl")
+include("algorithms.jl")
+include("solve.jl")
+
 export C1, C2, C3, C4, C5, C6, C7
+export commutator, eig_factors, expm_exact_gradient, trace_matmul
+export pw_evolve, pw_evolve_save, pw_evolve_T, pw_ham_save
+export algorithm, gradientBased, gradientFree, GRAPE_approx, GRAPE_AD, dCRAB_type, ADGRAPE, GRAPE, dCRAB
+export solve
+
 
 end

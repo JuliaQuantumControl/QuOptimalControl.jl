0c7194662546a8fb79ef0f87d09c8a0664f4f422
Alastair Marshall <alastair@nvision-imaging.com>
2020-08-25 13:15:01 +0200

    Updates before I start writing tests

--- src/QuOptimalControl.jl
+++ src/QuOptimalControl.jl
@@ -11,7 +11,7 @@ include("visualisation.jl")
 
 export C1, C2, C3, C4, C5, C6, C7, fom_func
 export commutator, eig_factors, expm_exact_gradient, trace_matmul, save, load
-export pw_evolve, pw_evolve_save, pw_evolve_T, pw_ham_save
+export pw_evolve, pw_evolve_save, pw_evolve_T, pw_ham_save, pw_ham_save!, pw_gen_save!, evolve_func, evolve_func!
 export algorithm, gradientBased, gradientFree, GRAPE_approx, GRAPE_AD, dCRAB_options, ADGRAPE, GRAPE, dCRAB, init_GRAPE, GRAPE_new, GRAPE!
 export solve
 export grad_func

e5c35f3864ff3dc1f47be439e079c555e0559f61
Alastair Marshall <alastair@nvision-imaging.com>
2020-09-03 12:14:05 +0200

    think we can avoid allocations here with @views?

--- src/algorithms.jl
+++ src/algorithms.jl
@@ -28,7 +28,7 @@ Compute the gradient of a functional f(x) wrt. x
 function ADGRAPE(functional, x; g_tol = 1e-6)
 
     function grad_functional!(G, x)
-        G .= Zygote.gradient(functional, x)[1]
+        @views G .= Zygote.gradient(functional, x)[1]
     end
     
     res = optimize(functional, grad_functional!, x, LBFGS(), Optim.Options(g_tol = g_tol, show_trace = true, store_trace = true))

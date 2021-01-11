fcd8182fcd5892be4898f45eca6e69f42668a6bf
alastair-marshall <40397029+alastair-marshall@users.noreply.github.com>
2021-01-11 12:25:30 +0100

    prep for ensemble optimisation rework

--- src/problems.jl
+++ src/problems.jl
@@ -8,7 +8,7 @@ import Base.@kwdef # unsure about using this so much
 """
 Description of the dynamics of a closed state transfer problem. This also applies method also applies to optimisation of Hermitian operators
 """
-@kwdef struct ClosedStateTransfer <: ClosedSystem
+@kwdef struct StateTransferProblem <: ClosedSystem
     B # control terms
     A # drift terms
     X_init # initial state
@@ -16,15 +16,13 @@ Description of the dynamics of a closed state transfer problem. This also applie
     duration # duration of pulse
     n_timeslices # slices
     n_controls # number of pulses
-    n_ensemble # number of members for ensemble optimisation
     initial_guess # guess at controls
 end
-
 
 """
 Description of the dynamics of a unitary synthesis problem. Although this contains the same information as another closed state problem by defining it separately we can choose the evolution methods that make sense for the problem
 """
-@kwdef struct UnitarySynthesis <: ClosedSystem
+@kwdef struct UnitaryProblem <: ClosedSystem
     B # control terms
     A # drift terms
     X_init # initial state
@@ -32,8 +30,13 @@ Description of the dynamics of a unitary synthesis problem. Although this contai
     duration # duration of pulse
     n_timeslices # slices
     n_controls # number of pulses
-    n_ensemble # number of members for ensemble optimisation
     initial_guess # guess at controls
+end
+
+"""
+Define an ensemble of problems, can provide any method for computing the new drift or control Hamiltonians
+"""
+@kwdef struct ClosedEnsembleProblem
 end
 
 """
@@ -49,7 +52,6 @@ Following the Khaneja and Glaser paper provided we can use the same gradient and
     duration # duration of pulse
     n_timeslices # slices
     n_controls # number of pulses
-    n_ensemble # number of members for ensemble optimisation
     initial_guess # guess at controls
 end
 

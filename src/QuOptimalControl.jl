3957fce5792d4ea77d93f5a45c00f038e931992d
Alastair Marshall <alastair@nvision-imaging.com>
2021-04-16 14:27:38 +0200

    doot doot

--- src/QuOptimalControl.jl
+++ src/QuOptimalControl.jl
@@ -15,11 +15,35 @@ include("visualisation.jl")
 export C1, C2, C3, C4, C5, C6, C7, fom_func
 export init_GRAPE, init_ensemble
 export commutator, eig_factors, expm_exact_gradient, trace_matmul, save, load
-export pw_evolve, pw_evolve_save, pw_evolve_T, pw_ham_save, pw_ham_save!, pw_gen_save!, evolve_func, evolve_func!
-export algorithm, gradientBased, gradientFree, GRAPE_approx, GRAPE_AD, dCRAB_options, ADGRAPE, GRAPE, dCRAB, GRAPE_new, GRAPE!
+export pw_evolve,
+    pw_evolve_save,
+    pw_evolve_T,
+    pw_ham_save,
+    pw_ham_save!,
+    pw_gen_save!,
+    evolve_func,
+    evolve_func!
+export algorithm,
+    gradientBased,
+    gradientFree,
+    GRAPE_approx,
+    GRAPE_AD,
+    dCRAB_options,
+    ADGRAPE,
+    GRAPE,
+    dCRAB,
+    GRAPE_new,
+    GRAPE!
 export solve
 export grad_func
-export Problem, ClosedSystem, Experiment, StateTransferProblem, UnitaryProblem, ClosedEnsembleProblem, OpenSystemCoherenceTransfer, ExperimentInterface
+export Problem,
+    ClosedSystem,
+    Experiment,
+    StateTransferProblem,
+    UnitaryProblem,
+    ClosedEnsembleProblem,
+    OpenSystemCoherenceTransfer,
+    ExperimentInterface
 export visualise_expt_val, visualise_pulse
 
 end

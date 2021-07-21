1b477839d66b2e0250f8e9fb4fe248df7bcc3c7c
Alastair <alastair@nvision-imaging.com>
2021-07-21 21:58:37 +0200

    format

--- test/problem_tests.jl
+++ test/problem_tests.jl
@@ -12,16 +12,16 @@
         n_controls = 2,
         guess = rand(2, 10),
         sys_type = StateTransfer()
-    
+
     )
-    
+
     sol = solve(prob, GRAPE(n_slices = 10, isinplace=true))
-    
+
     @test sol.result.minimum - C1(ρfin, ρfin) < tol
     end
-    
-    @testset "StateTransfer GRAPE" begin
-    
+
+@testset "StateTransfer GRAPE" begin
+
     prob = Problem(
         B = [SSx, SSy],
         A = SSz,
@@ -32,16 +32,16 @@
         guess = rand(2, 10),
         sys_type = StateTransfer()
     )
-    
+
     sol = solve(prob, GRAPE(n_slices=10, isinplace=false))
     @test sol.result.minimum - C1(ρfin, ρfin) < tol
-    
-    end
-    
-    
-    
-    @testset "StateTransfer Ensemble inplace" begin
-    
+
+end
+
+
+
+@testset "StateTransfer Ensemble inplace" begin
+
     prob = StateTransferProblem(
         B = [Sx, Sy],
         A = Sz,
@@ -52,33 +52,33 @@
         n_controls = 2,
         initial_guess = rand(2, 25)
     )
-    
-    
+
+
     ens = ClosedEnsembleProblem(prob, 5, A_gens, B_gens, X_init_gens, X_target_gens, ones(5)/5)
-    
+
     sol = GRAPE(ens, inplace=true)
     @test sol.result[1].minimum - C1(ρfin, ρfin) < tol * 10
-    
-    end
-    
-    
-    
-    @testset "StateTransfer Ensemble out of place" begin
-        prob = StateTransferProblem(
-            B = [SSx, SSy],
-            A = SSz,
-            X_init = ρinitS,
-            X_target = ρfinS,
-            duration = 5.0,
-            n_timeslices = 25,
-            n_controls = 2,
-            initial_guess = rand(2, 25)
-        )
-    
-    
-        ens = ClosedEnsembleProblem(prob, 5, A_gens_static, B_gens_static, X_init_gens_static, X_target_gens_static, ones(5)/5)
-    
-        sol = GRAPE(ens, inplace=false)
-        @test sol.result[1].minimum - C1(ρfin, ρfin) < tol * 10
-    
-    end
+
+end
+
+
+
+@testset "StateTransfer Ensemble out of place" begin
+    prob = StateTransferProblem(
+        B = [SSx, SSy],
+        A = SSz,
+        X_init = ρinitS,
+        X_target = ρfinS,
+        duration = 5.0,
+        n_timeslices = 25,
+        n_controls = 2,
+        initial_guess = rand(2, 25)
+    )
+
+
+    ens = ClosedEnsembleProblem(prob, 5, A_gens_static, B_gens_static, X_init_gens_static, X_target_gens_static, ones(5)/5)
+
+    sol = GRAPE(ens, inplace=false)
+    @test sol.result[1].minimum - C1(ρfin, ρfin) < tol * 10
+
+end

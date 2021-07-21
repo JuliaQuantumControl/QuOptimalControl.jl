48bb5294e8c7a1263bac1ecf408eddc09c8eb659
Alastair <alastair@nvision-imaging.com>
2021-07-21 17:50:59 +0200

    starting to make changes to the problems

--- src/problems.jl
+++ src/problems.jl
@@ -1,77 +1,95 @@
-abstract type Problem end
-abstract type ClosedSystem <: Problem end
-abstract type OpenSystem <: Problem end
-abstract type Experiment <: Problem end
+abstract type SystemType end
+abstract type ClosedSystem <: SystemType end
+abstract type OpenSystem <: SystemType end
+abstract type Experiment <: SystemType end
 
 import Base.@kwdef # unsure about using this so much
 
-"""
-Description of the dynamics of a closed state transfer problem. This also applies method also applies to optimisation of Hermitian operators
-"""
-@kwdef struct StateTransferProblem{T} <: ClosedSystem
-    B::Vector{T} # control terms
-    A::T # drift terms
-    X_init::T # initial state
-    X_target::T # target operator or state
-    duration::Float64 # duration of pulse
-    n_timeslices::Int64 # slices
-    n_controls::Int64 # number of pulses
-    initial_guess::Matrix{Float64} # guess at controls
+
+# could do something like this
+# and then we do the dispatch on the system type rather than the problem itself
+@kwdef struct Problem{BT, AT, XT, T, NS, NC, IG, ST}
+    B_control::BT # control terms
+    A_drift::AT # drift terms
+    X_init::XT # initial state
+    X_target::XT # target operator or state
+    duration::T # duration of pulse
+    n_timeslices::NS # slices
+    n_controls::NC # number of pulses
+    initial_guess::IG # guess at controls
+    system_type::ST
 end
 
-"""
-Description of the dynamics of a unitary synthesis problem. Although this contains the same information as another closed state problem by defining it separately we can choose the evolution methods that make sense for the problem
-"""
-@kwdef struct UnitaryProblem <: ClosedSystem
-    B # control terms
-    A # drift terms
-    X_init # initial state
-    X_target # target operator or state
-    duration # duration of pulse
-    n_timeslices # slices
-    n_controls # number of pulses
-    initial_guess # guess at controls
-end
 
-"""
-Define an ensemble of problems, can provide any method for computing the new drift or control Hamiltonians
-"""
-@kwdef struct ClosedEnsembleProblem{T}
-    problem::T # for an ensemble of systems you'll provide one "template" problem
-    n_ensemble::Int64 # we need to know the number of ensemble members
-    A_generators # need to find a better name but these will give us some discretisation
-    B_generators # similarly to above
-    X_init_generators
-    X_target_generators
-    weights::Vector{Float64} # weights for calculating the figure of merit and gradient
-end
+
+
+# """
+# Description of the dynamics of a closed state transfer problem. This also applies method also applies to optimisation of Hermitian operators
+# """
+# @kwdef struct StateTransferProblem{BT, AT, XT, T, NS, NC, IG} <: ClosedSystem
+#     B_control::BT # control terms
+#     A_drift::AT # drift terms
+#     X_init::XT # initial state
+#     X_target::XT # target operator or state
+#     duration::T # duration of pulse
+#     n_timeslices::NS # slices
+#     n_controls::NC # number of pulses
+#     initial_guess::IG # guess at controls
+# end
+
+# """
+# Description of the dynamics of a unitary synthesis problem. Although this contains the same information as another closed state problem by defining it separately we can choose the evolution methods that make sense for the problem
+# """
+# @kwdef struct UnitaryProblem{BT, AT, XT, T, NS, NC, IG} <: ClosedSystem
+#     B_control::BT # control terms
+#     A_drift::AT # drift terms
+#     X_init::XT # initial state
+#     X_target::XT # target operator or state
+#     duration::T # duration of pulse
+#     n_timeslices::NS # slices
+#     n_controls::NC # number of pulses
+#     initial_guess::IG # guess at controls
+# end
+
+# """
+# Define an ensemble of problems, can provide any method for computing the new drift or control Hamiltonians
+# """
+# @kwdef struct ClosedEnsembleProblem{T, NE, AG, BG, XIG, XTG, WTS}
+#     problem::T # for an ensemble of systems you'll provide one "template" problem
+#     n_ensemble::NE # we need to know the number of ensemble members
+#     A_generators::AG # need to find a better name but these will give us some discretisation
+#     B_generators::BG # similarly to above
+#     X_init_generators::XIG
+#     X_target_generators::XTG
+#     weights::WTS # weights for calculating the figure of merit and gradient
+# end
 
 """
 Working in Liouville space we can do an optimisation in the presence of relaxation, we can also reuse the gradient and goal functions from before. Assuming Hermitian operators on input, need to deal with non-Hermitian.
 
 Following the Khaneja and Glaser paper provided we can use the same gradient and fom functions as in the relaxation free case but the evolution is now governed by the Liovuillian superoperator.
-"""
-@kwdef struct OpenSystemCoherenceTransfer <: OpenSystem
-    B # control terms
-    A # drift terms
-    X_init # initial state
-    X_target # target operator or state
-    duration # duration of pulse
-    n_timeslices # slices
-    n_controls # number of pulses
-    initial_guess # guess at controls
-end
+# """
+# @kwdef struct OpenSystemCoherenceTransfer{BT, AT, XT, T, NS, NC, IG} <: OpenSystem
+#     B_control::BT # control terms
+#     A_drift::AT # drift terms
+#     X_init::XT # initial state
+#     X_target::XT # target operator or state
+#     duration::T # duration of pulse
+#     n_timeslices::NS # slices
+#     n_controls::NC # number of pulses
+#     initial_guess::IG # guess at controls
+# end
 
 """
 Working with an experiment
 """
-@kwdef struct ExperimentInterface <: Experiment
-    duration
-    timestep
-    n_timeslices
-    n_controls
-    start_exp # function to start exp
-    pulse_path
-    infidelity_path
-    timeout
+@kwdef struct ExperimentInterface{T, TS, NS, NC, FS, PP, IP, TO} <: Experiment
+    duration::T
+    timestep::TS
+    n_timeslices::NS
+    n_controls::NC
+    start_exp::FS # function to start exp
+    pulse_path::PP
+    infidelity_path::IP
+    timeout::TO
 end
--- src/solve.jl
+++ src/solve.jl
@@ -2,223 +2,239 @@ using Optim
 using FileWatching
 using DelimitedFiles
 
-"""
-Contains the solve function interfaces for now, until I learn a better way to do it
-"""
-function GRAPE(prob::Union{StateTransferProblem,UnitaryProblem}; inplace = true, optim_options=Optim.Options())
-    if inplace
-        GRAPE!(prob, optim_options)
-    else
-        sGRAPE(prob, optim_options)
-    end
-end
-
-"""
-Handles solving ensemble problems with GRAPE
-"""
-function GRAPE(ens::ClosedEnsembleProblem; inplace = true, optim_options = Optim.Options())
-    if inplace
-        ensemble_GRAPE!(ens, optim_options)
-    else
-        ensemble_sGRAPE(ens, optim_options)
-    end
-end
-
-"""
-GRAPE!
-
-This function solves the single problem using Optim and the gradient defined for the problem type.
-"""
-function GRAPE!(problem, optim_options)
-
-    # initialise holding arrays for inplace operations, these will be modified
-    state_store, costate_store, propagators, fom, gradient = init_GRAPE(problem.X_init, problem.n_timeslices, 1, problem.A, problem.n_controls)
-
-    evolve_store = similar(state_store[1]) .* 0.0
-    grad = @views gradient[1,:,:]
-
-    function _to_optim!(F, G, x)
-        fom = _fom_and_gradient_GRAPE!(problem.A, problem.B, x, problem.n_timeslices, problem.duration, problem.n_controls, grad, state_store, costate_store, propagators, problem.X_init, problem.X_target, evolve_store, problem)
-
-        if G !== nothing
-            @views G .= grad
-        end
-        if F !== nothing
-            return fom
-        end
-    end
 
 
-    init = problem.initial_guess
-
-    res = Optim.optimize(Optim.only_fg!(_to_optim!), init, Optim.LBFGS(), optim_options)
-    solres = SolutionResult([res], [res.minimum], [res.minimizer], problem)
-    return solres
-end
-
-"""
-sGRAPE
-
-This function solves the single problem defined in problem using Optim and the gradient for the problem type. Use this version with static arrays for 0 allocations!
-"""
-function sGRAPE(problem, optim_options)
-    # prepare some storage arrays that we will make use of throughout the computation
-    Ut = Vector{typeof(problem.A)}(undef, problem.n_timeslices + 1)
-    Lt = Vector{typeof(problem.A)}(undef, problem.n_timeslices + 1)
-    gradient = zeros(problem.n_controls, problem.n_timeslices)
-
-    function _to_optim!(F, G, x)
-        grad = @views gradient[:, :]
-        fom = _fom_and_gradient_sGRAPE(problem.A, problem.B, x, problem.n_timeslices, problem.duration, problem.n_controls, grad, Ut, Lt, problem.X_init, problem.X_target, problem)
-
-        if G !== nothing
-            @views G .= grad
-        end
-        if F !== nothing
-            return fom
-        end
-    end
-
-    init = problem.initial_guess
-
-    res = Optim.optimize(Optim.only_fg!(_to_optim!), init, Optim.LBFGS(), optim_options)
-    solres = SolutionResult([res], [res.minimum], [res.minimizer], problem)
-
-    return solres
-end
 
 
-"""
-ensemble_GRAPE!
-
-This function solves the ensemble problem defined in ensemble_problem using the mutating GRAPE algorithm for efficiency.
-"""
-function ensemble_GRAPE!(ensemble_problem, optim_options)
-    # we expand the ensemble problem into an array of single problems that we can solve
-    ensemble_problem_array = init_ensemble(ensemble_problem)
-
-    # to initialise the holding arrays we define a problem
-    problem = ensemble_problem_array[1]
-    # initialise holding arrays for inplace operations, these will be modified
-    state_store, costate_store, propagators, fom, gradient = init_GRAPE(problem.X_init, problem.n_timeslices, ensemble_problem.n_ensemble, problem.A, problem.n_controls)
-
-    evolve_store = similar(state_store[1]) .* 0.0
-    weights = ensemble_problem.weights
-    n_ensemble = ensemble_problem.n_ensemble
-
-    function _to_optim!(F, G, x)
-        fom = 0.0
-        for k = 1:n_ensemble
-            problem = ensemble_problem_array[k]
-
-            fom += @views _fom_and_gradient_GRAPE!(problem.A, problem.B, x, problem.n_timeslices, problem.duration, problem.n_controls, gradient[k, :, :], state_store[:, k], costate_store[:, k], propagators[:,k], problem.X_init, problem.X_target, evolve_store, problem) * weights[k]
-        end
-
-        if G !== nothing
-            # TODO is this performant?
-            @views G .= sum(gradient .* weights, dims = 1)[1,:,:]
-        end
-        if F !== nothing
-            return fom
-        end
-    end
 
 
-    init = problem.initial_guess
-
-    res = Optim.optimize(Optim.only_fg!(_to_optim!), init, Optim.LBFGS(), optim_options)
-    solres = SolutionResult([res], [res.minimum], [res.minimizer], problem)
-    return solres
-end
 
 
-"""
-ensemble_sGRAPE
 
-This function solves the ensemble problem defined in ensemble_problem using the non-mutating version of the GRAPE algorithm.
-"""
-function ensemble_sGRAPE(ensemble_problem, optim_options)
-    ensemble_problem_array = init_ensemble(ensemble_problem)
 
-    problem = ensemble_problem_array[1]
 
-    Ut = Vector{typeof(problem.A)}(undef, problem.n_timeslices + 1)
-    Lt = Vector{typeof(problem.A)}(undef, problem.n_timeslices + 1)
 
-    gradient = zeros(ensemble_problem.n_ensemble, problem.n_controls, problem.n_timeslices)
-
-    n_ensemble = ensemble_problem.n_ensemble
-    weights = ensemble_problem.weights
-
-    function _to_optim!(F, G, x)
-        fom = 0.0
-            for k = 1:n_ensemble
-                problem = ensemble_problem_array[k]
-                grad = @views gradient[k, :, :]
-
-                fom += _fom_and_gradient_sGRAPE(problem.A, problem.B, x, problem.n_timeslices, problem.duration, problem.n_controls, grad, Ut, Lt, problem.X_init, problem.X_target, problem) * weights[k]
-
-            if G !== nothing
-                @views G .= sum(gradient .* weights, dims = 1)[1,:,:]
-            end
-            if F !== nothing
-                return fom
-            end
-        end
-    end
-
-    init = problem.initial_guess
-
-    res = Optim.optimize(Optim.only_fg!(_to_optim!), init, Optim.LBFGS(), optim_options)
-    solres = SolutionResult([res], [res.minimum], [res.minimizer], problem)
-
-    return solres
-end
 
 
 
 
 # """
-# Solve closed state transfer problem using ADGRAPE, this means that we need to use a piecewise evolution function that is Zygote compatible!
+# Contains the solve function interfaces for now, until I learn a better way to do it
 # """
-# function ADGRAPE(prob::StateTransferProblem; optim_options = Optim.Options())
-#     wts = ones(prob.n_ensemble)
-#     D = size(prob.A[1])[1]
-#     u0 = typeof(prob.A[1])(I(D))
-
-#     function user_functional(x)
-#         fom = 0.0
-#         for k = 1:prob.n_ensemble
-#             U = pw_evolve_T(prob.A[k], prob.B[k], x, prob.n_controls, prob.duration/prob.n_timeslices, prob.n_timeslices, u0)
-#             fom += C1(prob.X_target[k], (U * prob.X_init[k] * U')) * wts[k]
-#         end
-#         fom
+# function GRAPE(prob::Union{StateTransferProblem,UnitaryProblem}; inplace = true, optim_options=Optim.Options())
+#     if inplace
+#         GRAPE!(prob, optim_options)
+#     else
+#         sGRAPE(prob, optim_options)
 #     end
-
-#     init = prob.initial_guess
-#     res = _ADGRAPE(user_functional, init, optim_options=optim_options)
-
-#     solres = SolutionResult([res], [res.minimum], [res.minimizer], prob)
 # end
 
 # """
-# Solve a unitary synthesis prob using ADGRAPE, this means that we need to use a piecewise evolution function that is Zygote compatible!
+# Handles solving ensemble problems with GRAPE
 # """
-# function ADGRAPE(prob::UnitaryProblem; optim_options = Optim.Options())
-#     wts = ones(prob.n_ensemble)
-#     u0 = typeof(prob.A[1])(I(D))
+# function GRAPE(ens::ClosedEnsembleProblem; inplace = true, optim_options = Optim.Options())
+#     if inplace
+#         ensemble_GRAPE!(ens, optim_options)
+#     else
+#         ensemble_sGRAPE(ens, optim_options)
+#     end
+# end
 
-#     function user_functional(x)
-#         fom = 0.0
-#         for k = 1:prob.n_ensemble
-#             U = pw_evolve_T(prob.A[k], prob.B[k], x, prob.n_controls, prob.duration/prob.n_timeslices, prob.n_timeslices, u0)
-#             fom += C1(prob.X_target[k], U * prob.X_init[k]) * wts[k]
+# """
+# GRAPE!
+
+# This function solves the single problem using Optim and the gradient defined for the problem type.
+# """
+# function GRAPE!(problem, optim_options)
+
+#     # initialise holding arrays for inplace operations, these will be modified
+#     state_store, costate_store, propagators, fom, gradient = init_GRAPE(problem.X_init, problem.n_timeslices, 1, problem.A, problem.n_controls)
+
+#     evolve_store = similar(state_store[1]) .* 0.0
+#     grad = @views gradient[1,:,:]
+
+#     function _to_optim!(F, G, x)
+#         fom = _fom_and_gradient_GRAPE!(problem.A, problem.B, x, problem.n_timeslices, problem.duration, problem.n_controls, grad, state_store, costate_store, propagators, problem.X_init, problem.X_target, evolve_store, problem)
+
+#         if G !== nothing
+#             @views G .= grad
 #         end
-#         fom
+#         if F !== nothing
+#             return fom
+#         end
 #     end
 
-#     init = prob.initial_guess
-#     res = _ADGRAPE(user_functional, init, optim_options=optim_options)
 
-#     solres = SolutionResult([res], [res.minimum], [res.minimizer], prob)
+#     init = problem.initial_guess
+
+#     res = Optim.optimize(Optim.only_fg!(_to_optim!), init, Optim.LBFGS(), optim_options)
+#     solres = SolutionResult([res], [res.minimum], [res.minimizer], problem)
+#     return solres
 # end
+
+# """
+# sGRAPE
+
+# This function solves the single problem defined in problem using Optim and the gradient for the problem type. Use this version with static arrays for 0 allocations!
+# """
+# function sGRAPE(problem, optim_options)
+#     # prepare some storage arrays that we will make use of throughout the computation
+#     Ut = Vector{typeof(problem.A)}(undef, problem.n_timeslices + 1)
+#     Lt = Vector{typeof(problem.A)}(undef, problem.n_timeslices + 1)
+#     gradient = zeros(problem.n_controls, problem.n_timeslices)
+
+#     function _to_optim!(F, G, x)
+#         grad = @views gradient[:, :]
+#         fom = _fom_and_gradient_sGRAPE(problem.A, problem.B, x, problem.n_timeslices, problem.duration, problem.n_controls, grad, Ut, Lt, problem.X_init, problem.X_target, problem)
+
+#         if G !== nothing
+#             @views G .= grad
+#         end
+#         if F !== nothing
+#             return fom
+#         end
+#     end
+
+#     init = problem.initial_guess
+
+#     res = Optim.optimize(Optim.only_fg!(_to_optim!), init, Optim.LBFGS(), optim_options)
+#     solres = SolutionResult([res], [res.minimum], [res.minimizer], problem)
+
+#     return solres
+# end
+
+
+# """
+# ensemble_GRAPE!
+
+# This function solves the ensemble problem defined in ensemble_problem using the mutating GRAPE algorithm for efficiency.
+# """
+# function ensemble_GRAPE!(ensemble_problem, optim_options)
+#     # we expand the ensemble problem into an array of single problems that we can solve
+#     ensemble_problem_array = init_ensemble(ensemble_problem)
+
+#     # to initialise the holding arrays we define a problem
+#     problem = ensemble_problem_array[1]
+#     # initialise holding arrays for inplace operations, these will be modified
+#     state_store, costate_store, propagators, fom, gradient = init_GRAPE(problem.X_init, problem.n_timeslices, ensemble_problem.n_ensemble, problem.A, problem.n_controls)
+
+#     evolve_store = similar(state_store[1]) .* 0.0
+#     weights = ensemble_problem.weights
+#     n_ensemble = ensemble_problem.n_ensemble
+
+#     function _to_optim!(F, G, x)
+#         fom = 0.0
+#         for k = 1:n_ensemble
+#             problem = ensemble_problem_array[k]
+
+#             fom += @views _fom_and_gradient_GRAPE!(problem.A, problem.B, x, problem.n_timeslices, problem.duration, problem.n_controls, gradient[k, :, :], state_store[:, k], costate_store[:, k], propagators[:,k], problem.X_init, problem.X_target, evolve_store, problem) * weights[k]
+#         end
+
+#         if G !== nothing
+#             # TODO is this performant?
+#             @views G .= sum(gradient .* weights, dims = 1)[1,:,:]
+#         end
+#         if F !== nothing
+#             return fom
+#         end
+#     end
+
+
+#     init = problem.initial_guess
+
+#     res = Optim.optimize(Optim.only_fg!(_to_optim!), init, Optim.LBFGS(), optim_options)
+#     solres = SolutionResult([res], [res.minimum], [res.minimizer], problem)
+#     return solres
+# end
+
+
+# """
+# ensemble_sGRAPE
+
+# This function solves the ensemble problem defined in ensemble_problem using the non-mutating version of the GRAPE algorithm.
+# """
+# function ensemble_sGRAPE(ensemble_problem, optim_options)
+#     ensemble_problem_array = init_ensemble(ensemble_problem)
+
+#     problem = ensemble_problem_array[1]
+
+#     Ut = Vector{typeof(problem.A)}(undef, problem.n_timeslices + 1)
+#     Lt = Vector{typeof(problem.A)}(undef, problem.n_timeslices + 1)
+
+#     gradient = zeros(ensemble_problem.n_ensemble, problem.n_controls, problem.n_timeslices)
+
+#     n_ensemble = ensemble_problem.n_ensemble
+#     weights = ensemble_problem.weights
+
+#     function _to_optim!(F, G, x)
+#         fom = 0.0
+#             for k = 1:n_ensemble
+#                 problem = ensemble_problem_array[k]
+#                 grad = @views gradient[k, :, :]
+
+#                 fom += _fom_and_gradient_sGRAPE(problem.A, problem.B, x, problem.n_timeslices, problem.duration, problem.n_controls, grad, Ut, Lt, problem.X_init, problem.X_target, problem) * weights[k]
+
+#             if G !== nothing
+#                 @views G .= sum(gradient .* weights, dims = 1)[1,:,:]
+#             end
+#             if F !== nothing
+#                 return fom
+#             end
+#         end
+#     end
+
+#     init = problem.initial_guess
+
+#     res = Optim.optimize(Optim.only_fg!(_to_optim!), init, Optim.LBFGS(), optim_options)
+#     solres = SolutionResult([res], [res.minimum], [res.minimizer], problem)
+
+#     return solres
+# end
+
+
+
+
+# # """
+# # Solve closed state transfer problem using ADGRAPE, this means that we need to use a piecewise evolution function that is Zygote compatible!
+# # """
+# # function ADGRAPE(prob::StateTransferProblem; optim_options = Optim.Options())
+# #     wts = ones(prob.n_ensemble)
+# #     D = size(prob.A[1])[1]
+# #     u0 = typeof(prob.A[1])(I(D))
+
+# #     function user_functional(x)
+# #         fom = 0.0
+# #         for k = 1:prob.n_ensemble
+# #             U = pw_evolve_T(prob.A[k], prob.B[k], x, prob.n_controls, prob.duration/prob.n_timeslices, prob.n_timeslices, u0)
+# #             fom += C1(prob.X_target[k], (U * prob.X_init[k] * U')) * wts[k]
+# #         end
+# #         fom
+# #     end
+
+# #     init = prob.initial_guess
+# #     res = _ADGRAPE(user_functional, init, optim_options=optim_options)
+
+# #     solres = SolutionResult([res], [res.minimum], [res.minimizer], prob)
+# # end
+
+# # """
+# # Solve a unitary synthesis prob using ADGRAPE, this means that we need to use a piecewise evolution function that is Zygote compatible!
+# # """
+# # function ADGRAPE(prob::UnitaryProblem; optim_options = Optim.Options())
+# #     wts = ones(prob.n_ensemble)
+# #     u0 = typeof(prob.A[1])(I(D))
+
+# #     function user_functional(x)
+# #         fom = 0.0
+# #         for k = 1:prob.n_ensemble
+# #             U = pw_evolve_T(prob.A[k], prob.B[k], x, prob.n_controls, prob.duration/prob.n_timeslices, prob.n_timeslices, u0)
+# #             fom += C1(prob.X_target[k], U * prob.X_init[k]) * wts[k]
+# #         end
+# #         fom
+# #     end
+
+# #     init = prob.initial_guess
+# #     res = _ADGRAPE(user_functional, init, optim_options=optim_options)
+
+# #     solres = SolutionResult([res], [res.minimum], [res.minimizer], prob)
+# # end
